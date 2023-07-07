8249ac360b88a7bbf6496658dec952dcd993c69b
Salih Yusuf Göktaş <sygoktas@outlook.com.tr>
2023-07-07 22:01:46 +0300

    InstagramClone

---
+++ instagramClone/VC/FeedCell.swift
@@ -0,0 +1,42 @@
+//
+//  FeedCell.swift
+//  instagramClone
+//
+//  Created by Salih Yusuf Göktaş on 7.07.2023.
+//
+
+import UIKit
+import Firebase
+import FirebaseFirestore
+
+class FeedCell: UITableViewCell {
+
+	@IBOutlet weak var userEmailLabel: UILabel!
+	@IBOutlet weak var commentLabel: UILabel!
+	@IBOutlet weak var userImageView: UIImageView!
+	@IBOutlet weak var likeLabel: UILabel!
+	@IBOutlet weak var documentIDLabel: UILabel!
+	
+	
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+	@IBAction func likeButtonClicked(_ sender: Any) {
+		
+		let fireStoreDatabase = Firestore.firestore()
+		if let likeCount = Int(likeLabel.text!) {
+
+			let likeStore = ["likes" : likeCount + 1] as [String : Any]
+
+		fireStoreDatabase.collection("Posts").document(documentIDLabel.text!).setData(likeStore, merge: true)
+	
+		}
+	}
+}
---
+++ instagramClone/VC/SettingsViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SettingsViewController.swift
+//  instagramClone
+//
+//  Created by Salih Yusuf Göktaş on 6.07.2023.
+//
+
+import UIKit
+import Firebase
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+	@IBAction func logoutClicked(_ sender: Any) {
+		do {
+			try Auth.auth().signOut()
+			self.performSegue(withIdentifier: "toViewController", sender: nil)
+		} catch {
+			
+		}
+		
+	}
+}
